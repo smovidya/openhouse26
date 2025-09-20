df9bcc5a15bfc1ad752d2ec867459f78438a488d
Patsagorn <patsagorn_y@outlook.com>
2025-09-20 15:22:44 +0700

    Update compatibility date in wrangler configuration

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="astro/client" />
 
-type ENV = {
-};
-
 // use a default runtime configuration (advanced mode).
-type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
   interface Locals extends Runtime {}
 }
