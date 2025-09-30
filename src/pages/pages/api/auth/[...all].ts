0991672c7720b310bc097b30686fe23763a489ee
Patsagorn <patsagorn_y@outlook.com>
2025-10-01 01:54:46 +0700

    feat: add prerender flag to API route for improved performance

--- src/pages/pages/api/auth/[...all].ts
+++ src/pages/pages/api/auth/[...all].ts
@@ -1,6 +1,8 @@
 import { createAuth } from "@src/auth";
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 export const ALL: APIRoute = async (ctx) => {
   // If you want to use rate limiting, make sure to set the 'x-forwarded-for' header to the request headers from the context
   ctx.request.headers.set("x-forwarded-for", ctx.clientAddress);
