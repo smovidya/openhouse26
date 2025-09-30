325589641ca83c1692dcd28b2828ff5b5a108f89
Patsagorn <patsagorn_y@outlook.com>
2025-10-01 01:54:56 +0700

    refactor: simplify fetch handler structure in createExports function

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -12,15 +12,14 @@ import { handle } from "@astrojs/cloudflare/handler";
 export function createExports(manifest: SSRManifest) {
   const app = new App(manifest);
   return {
-    default: {
-      async fetch(request, env, ctx) {
-        return handle(manifest, app, request, env, ctx);
-      },
-      // async queue(batch, _env) {
-      //   let messages = JSON.stringify(batch.messages);
-      //   console.log(`consumed from our queue: ${messages}`);
-      // },
-    } satisfies ExportedHandler<Env>,
-    // MyDurableObject: MyDurableObject,
-  };
+    async fetch(request, env, ctx) {
+      // @ts-ignore
+      return handle(manifest, app, request, env, ctx);
+    },
+    // async queue(batch, _env) {
+    //   let messages = JSON.stringify(batch.messages);
+    //   console.log(`consumed from our queue: ${messages}`);
+    // },
+  } satisfies ExportedHandler<Env>;
+  // MyDurableObject: MyDurableObject,
 }
