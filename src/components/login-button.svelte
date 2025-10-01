a17e37bada7168e2c3c752a019ed9265e4217dac
Patsagorn <patsagorn_y@outlook.com>
2025-10-01 13:34:30 +0700

    feat: add Google social sign-in button component

---
+++ src/components/login-button.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { authClientSvelte } from "@src/auth/client";
+</script>
+
+<button
+  onclick={async () => {
+    await authClientSvelte.signIn.social({
+      provider: "google",
+    });
+  }}
+>
+  ลงทะเบียนเข้าร่วมกิจกรรม
+</button>
