1299533921e7168bfb001d3b803ba6c5bb182a4b
Patsagorn <patsagorn_y@outlook.com>
2025-10-03 23:11:15 +0700

    feat: add cuid2 dependency and create helper schema for id and timestamps

---
+++ src/db/schema/helper.ts
@@ -0,0 +1,17 @@
+import { createId } from "@paralleldrive/cuid2";
+import { integer, text } from "drizzle-orm/sqlite-core";
+
+export const id = {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+};
+
+export const timestamps = {
+  createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(
+    () => new Date()
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .$defaultFn(() => new Date())
+    .$onUpdateFn(() => new Date()),
+};
