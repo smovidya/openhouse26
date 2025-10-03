e6fe3577fc4e10ec9d38f80b0f3ac4e4fe96a9ab
ongsalt <me@ongsa.lt>
2025-10-03 14:45:19 +0700

    add some common component

---
+++ src/components/button.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import clsx from "clsx";
+  import type { Snippet } from "svelte";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  interface Props {
+    /**
+     * Label for the button
+     */
+    children?: Snippet;
+    color: "yellow";
+  }
+
+  const {
+    children,
+    color,
+    class: clazz,
+    ...props
+  }: Props & HTMLAttributes<HTMLButtonElement> = $props();
+</script>
+
+<button
+  type="button"
+  class={clsx(
+    "w-full p-4 rounded-xl border border-[#fff4da64] shadow-lg shadow-black/30",
+    color,
+    clazz,
+  )}
+  {...props}
+>
+  {@render children?.()}
+</button>
+
+<style>
+  .yellow {
+    background: linear-gradient(
+      180deg,
+      #ffb843 0%,
+      #e99100 37%,
+      #dd8900 52%,
+      #ec9300 70%,
+      #ffb53d 100%
+    );
+
+    &:hover, &:active {
+      background: linear-gradient(
+        180deg,
+        #ea9200 1.58%,
+        #d26c00 29.98%,
+        #c06c06 51.86%,
+        #e17400 69.55%,
+        #e99100 98.42%
+      );
+    }
+  }
+</style>
---
+++ src/components/cutout-box.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import clsx, { type ClassValue } from "clsx";
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    class?: ClassValue;
+    a: number;
+    children?: Snippet;
+  }
+
+  let { class: clazz, children, a }: Props = $props();
+</script>
+
+<div
+  class={clsx(
+    "border-neutral-500 border-1 rounded-xl bg-white p-4 shadow-inner shadow-black/30",
+    clazz,
+  )}
+>
+  {a}
+  {@render children?.()}
+</div>
