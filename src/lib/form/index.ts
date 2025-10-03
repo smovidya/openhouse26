6d51ac6d47f60f34702caaf90d069075cc620dc1
ongsalt <me@ongsa.lt>
2025-10-04 02:43:13 +0700

    remove unused code

--- src/lib/form/index.ts
+++
@@ -1,6 +0,0 @@
-import { FormState, type FormConfig } from "./form-state.svelte";
-
-export function createForm<T extends Record<string, any>>(config: FormConfig<T>): FormState<T> {
-  return new FormState(config);
-}
-
