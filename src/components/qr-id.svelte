f9dff8a105d6e93e2ba15d2f94ba1b2975e069aa
ongsalt <me@ongsa.lt>
2025-10-04 14:18:40 +0700

    add qrcode

---
+++ src/components/qr-id.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { toSVG } from "@bwip-js/generic";
+  import clsx from "clsx";
+  interface Props {
+    class?: any;
+    content: string;
+  }
+
+  let { class: className, content }: Props = $props();
+
+  let svg = $derived(
+    toSVG({
+      text: content,
+      bcid: "azteccode",
+      // bcid: "azteccodecompact",
+    }),
+  );
+</script>
+
+<div class={clsx("bg-white", className)}>
+  {@html svg}
+</div>
