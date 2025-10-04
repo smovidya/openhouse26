ed4b712a77525b3583af2333553237b26a60ab57
Patsagorn <patsagorn_y@outlook.com>
2025-10-04 16:44:42 +0700

    feat: add logout button component with sign-out functionality

---
+++ src/components/logout-button.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { authClientSvelte } from "@src/auth/client";
+  import Button from "./common/button.svelte";
+
+  const { children } = $props();
+</script>
+
+<Button
+  onclick={async () => {
+    await authClientSvelte.signOut();
+    alert("คุณได้ออกจากระบบแล้ว");
+  }}
+>
+  {@render children?.()}
+</Button>
