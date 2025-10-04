54bb9694ceb06c00ef9ca41c93dedb18c102224d
Patsagorn <patsagorn_y@outlook.com>
2025-10-04 12:57:28 +0700

    feat: update departments data with correct names and IDs

--- src/data/departments.ts
+++ src/data/departments.ts
@@ -7,14 +7,88 @@ export interface Department {
 
 export const departments = [
   {
-    enShortName: "testtt",
-    thName: "คณะรักษาความสงบแห่งชาติ",
-    id: 12
+    enShortName: "MathCom",
+    thName: "คณิตศาสตร์และวิทยาการคอมพิวเตอร์",
+    id: 1,
   },
   {
-    enShortName: "testtt2",
-    thName: "คณะรัฐมนตรี",
-    id: 1223
-  }
-
+    enShortName: "Chem",
+    thName: "เคมี",
+    id: 2,
+  },
+  {
+    enShortName: "BioZoo",
+    thName: "ชีววิทยา",
+    id: 3,
+  },
+  {
+    enShortName: "Physics",
+    thName: "ฟิสิกส์",
+    id: 4,
+  },
+  {
+    enShortName: "Botany",
+    thName: "พฤกษศาสตร์",
+    id: 5,
+  },
+  {
+    enShortName: "ChemTech",
+    thName: "เคมีเทคนิค",
+    id: 6,
+  },
+  {
+    enShortName: "Geology",
+    thName: "ธรณีวิทยา",
+    id: 7,
+  },
+  {
+    enShortName: "EnviSci",
+    thName: "วิทยาศาสตร์สิ่งแวดล้อม",
+    id: 8,
+  },
+  {
+    enShortName: "MarineSci",
+    thName: "วิทยาศาสตร์ทางทะเล",
+    id: 9,
+  },
+  {
+    enShortName: "BioChem",
+    thName: "ชีวเคมี",
+    id: 10,
+  },
+  {
+    enShortName: "MatSci",
+    thName: "วัสดุศาสตร์",
+    id: 11,
+  },
+  {
+    enShortName: "MicroBio",
+    thName: "จุลชีววิทยา",
+    id: 12,
+  },
+  {
+    enShortName: "ImPrint",
+    thName: "เทคโนโลยีทางภาพและการพิมพ์",
+    id: 13,
+  },
+  {
+    enShortName: "FoodTech",
+    thName: "เทคโนโลยีทางอาหาร",
+    id: 14,
+  },
+  {
+    enShortName: "BSAC",
+    thName: "เคมีประยุกต์ (นานาชาติ)",
+    id: 15,
+  },
+  {
+    enShortName: "BBTech",
+    thName: "SIS (BBTech)",
+    id: 16,
+  },
+  {
+    enShortName: "BISTech",
+    thName: "SIS (BISTech)",
+    id: 17,
+  },
 ] as const satisfies Department[];
