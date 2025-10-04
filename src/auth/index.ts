04e8174a3155b6c3de8388617e7985bd0ed682e7
ongsalt <me@ongsa.lt>
2025-10-04 18:11:11 +0700

    fuck kv

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,20 +2,20 @@ import type {
   D1Database,
   IncomingRequestCfProperties,
 } from "@cloudflare/workers-types";
+import { schema } from "@src/db";
 import { betterAuth } from "better-auth";
 import { withCloudflare } from "better-auth-cloudflare";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin, oneTap } from "better-auth/plugins";
 import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
-import { schema } from "@src/db";
-import { admin, anonymous, oneTap } from "better-auth/plugins";
 import {
-  admin as adminRole,
   ac,
+  admin as adminRole,
   majorBoothStaff,
   registarStaff,
   rewardStaff,
-  workshopStaff,
   user,
+  workshopStaff,
 } from "./permissions";
 
 // Single auth configuration that handles both CLI and runtime scenarios
@@ -34,15 +34,25 @@ function createAuth(env?: Env, cf?: IncomingRequestCfProperties) {
         // @ts-ignore
         d1: env
           ? {
-              db,
-              options: {
-                usePlural: true,
-                debugLogs: true,
-              },
-            }
+            db,
+            options: {
+              usePlural: true,
+              debugLogs: true,
+            },
+          }
           : undefined,
         // @ts-ignore
-        kv: env?.openhouse26_kv,
+        kv: (
+          env?.openhouse26_kv ? {
+            ...env?.openhouse26_kv,
+            put(key, value, options) {
+              env.openhouse26_kv.put(key, value as any, {
+                ...options,
+                expirationTtl: Math.max(options?.expirationTtl ?? 120, 120)
+              });
+            },
+          } : undefined
+        ),
         // Optional: Enable R2 file storage
         // r2: {
         //   bucket: env.R2_BUCKET,
@@ -72,10 +82,10 @@ function createAuth(env?: Env, cf?: IncomingRequestCfProperties) {
         emailAndPassword: {
           enabled: true,
         },
-        // rateLimit: {
-        //   enabled: true,
-        //   window: 120
-        // },
+        rateLimit: {
+          enabled: true,
+          window: 120
+        },
         plugins: [
           admin({
             ac,
@@ -117,12 +127,12 @@ function createAuth(env?: Env, cf?: IncomingRequestCfProperties) {
     ...(env
       ? {}
       : {
-          database: drizzleAdapter({} as D1Database, {
-            provider: "sqlite",
-            usePlural: true,
-            debugLogs: true,
-          }),
+        database: drizzleAdapter({} as D1Database, {
+          provider: "sqlite",
+          usePlural: true,
+          debugLogs: true,
         }),
+      }),
   });
 }
 
