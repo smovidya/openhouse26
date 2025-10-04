1029b9ed2b81e9f4dbd2c18b61a8215bbb76f323
ongsalt <me@ongsa.lt>
2025-10-04 16:21:06 +0700

    add countdown timer

--- src/components/countdown.svelte
+++ src/components/countdown.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import CutoutBox from "./common/cutout-box.svelte";
+  import clsx from "clsx";
 
   interface Props {
     class?: any;
@@ -18,10 +20,10 @@
   let diffInSecond = $derived(targetInSecond - nowInSecond);
 
   let diff = $derived({
-    seconds: diffInSecond % 60,
-    minutes: (diffInSecond / 60) % 60,
-    hours: (diffInSecond / 3600) % 60,
-    days: (diffInSecond / (3600 * 24)) % 60,
+    seconds: Math.floor(diffInSecond % 60),
+    minutes: Math.floor(diffInSecond / 60) % 60,
+    hours: Math.floor(diffInSecond / 3600) % 24,
+    days: Math.floor(diffInSecond / (3600 * 24)) % 60,
   });
 
   onMount(() => {
@@ -33,4 +35,40 @@
   });
 </script>
 
-{JSON.stringify(diff)}
+<section class={clsx("flex gap-6 justify-center", className)}>
+  <div class="flex flex-col gap-2">
+    <CutoutBox class="w-16 rounded-lg! h-15 p-0! flex items-center justify-center">
+      <span class="text-black text-3xl font-bold font-serif">
+        {diff.days}
+      </span>
+    </CutoutBox>
+    <span class="text-sm"> วัน </span>
+  </div>
+
+  <div class="flex flex-col gap-2">
+    <CutoutBox class="w-16 rounded-lg! h-15 p-0! flex items-center justify-center">
+      <span class="text-black text-3xl font-bold font-serif">
+        {diff.hours}
+      </span>
+    </CutoutBox>
+    <span class="text-sm"> ชั่วโมง </span>
+  </div>
+
+  <div class="flex flex-col gap-2">
+    <CutoutBox class="w-16 rounded-lg! h-15 p-0! flex items-center justify-center">
+      <span class="text-black text-3xl font-bold font-serif">
+        {diff.minutes}
+      </span>
+    </CutoutBox>
+    <span class="text-sm"> นาที </span>
+  </div>
+
+  <div class="flex flex-col gap-2">
+    <CutoutBox class="w-16 rounded-lg! h-15 p-0! flex items-center justify-center">
+      <span class="text-black text-3xl font-bold font-serif">
+        {diff.seconds}
+      </span>
+    </CutoutBox>
+    <span class="text-sm"> วินาที </span>
+  </div>
+</section>
