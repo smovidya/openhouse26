c6335436532e1258c30cc8e812bb66b75589c4b6
ongsalt <me@ongsa.lt>
2025-10-06 03:10:54 +0700

    force disable signal value dedup

--- src/utils/state.svelte.ts
+++ src/utils/state.svelte.ts
@@ -1,7 +1,8 @@
 import { untrack } from "svelte";
 
 export class Debounced<T> {
-  #value: T = $state() as T;
+  #value: T;
+  #id = $state(0);
   #timeoutId: any = $state(null);
   debouncedTimeMs: number;
 
@@ -15,16 +16,18 @@ export class Debounced<T> {
   }
 
   get current() {
+    this.#id;
     return this.#value;
   }
 
   set current(value: T) {
-    const timeout = untrack(() => this.#timeoutId)
+    const timeout = untrack(() => this.#timeoutId);
     if (timeout) {
       clearTimeout(timeout);
     }
 
     this.#timeoutId = setTimeout(() => {
+      this.#id += 1;
       this.#value = value;
       this.#timeoutId = null;
     }, this.debouncedTimeMs);
