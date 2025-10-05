2879e816602d7b002a9c345386dd4a69b069e3b0
Patsagorn <patsagorn_y@outlook.com>
2025-10-05 13:30:49 +0700

    refactor: enhance Time class with string conversion methods and update Workshop interface structure

--- src/data/workshops.ts
+++ src/data/workshops.ts
@@ -6,21 +6,97 @@ class Time {
     this.#hour = hour;
     this.#minute = minute;
   }
+
+  toString() {
+    const h = this.#hour.toString().padStart(2, "0");
+    const m = this.#minute.toString().padStart(2, "0");
+    return `${h}:${m}`;
+  }
+
+  static fromString(timeStr: string) {
+    const [h, m] = timeStr.split(":").map(Number);
+    return new Time(h, m);
+  }
 }
 
-interface Period {
-  start: Time,
+interface TimeSlot {
+  round: number;
+  date: Date;
+  start: Time;
   end: Time;
-  afternoon: boolean;
 }
-
 
 interface Workshop {
-  id: number;
-  departmentId: number;
-  name: string;
+  id: string;
+  title: string;
   description: string;
-  maxParticipant: number;
-  rounds: Period[];
+  hostDepartment: string;
+  hostDepartmentAbbr: string;
+  image: string;
+  capacity: number;
+  slots: TimeSlot[];
 }
 
+export const workshops: Workshop[] = [
+  {
+    id: "kimchi",
+    title: "Kimchi Fermentation & 3D printing",
+    description:
+      "BBtech: Explore the origins of biotechnology through an everyday activity: making kimchi! Discover how microorganisms transform ingredients through fermentation, and learn how fermentation time affects flavor, texture, and nutritional value. Bistech: Discover how 3D printing is revolutionizing modern manufacturing and design. Learn about this innovative technology's real-world industrial applications and get hands-on experience assembling an actual 3D-printed project.",
+    hostDepartment:
+      "เทคโนโลยีชีวภาพ (นานาชาติ) ร่วมกับ หลักสูตรวิทยาศาสตร์และเทคโนโลยีอุตสาหการ (นานาชาติ)",
+    hostDepartmentAbbr: "BBTech & BISTech",
+    capacity: 15,
+    image: "/logo.png",
+    slots: [
+      {
+        round: 1,
+        date: new Date("2025-10-18"),
+        start: new Time(10, 10),
+        end: new Time(10, 50),
+      },
+      {
+        round: 2,
+        date: new Date("2025-10-18"),
+        start: new Time(11, 5),
+        end: new Time(11, 40),
+      },
+      {
+        round: 3,
+        date: new Date("2025-10-18"),
+        start: new Time(13, 30),
+        end: new Time(14, 10),
+      },
+      {
+        round: 4,
+        date: new Date("2025-10-18"),
+        start: new Time(14, 30),
+        end: new Time(15, 10),
+      },
+    ],
+  },
+  {
+    id: "banana-dna",
+    title: "A minion mission: Banana DNA",
+    description:
+      "กิจกรรมนี้ถูกออกแบบมาเพื่อเป็นประตูบานแรกสู่โลกแห่งชีวเคมีและการค้นพบทางวิทยาศาสตร์ ด้วยการใช้เพียงอุปกรณ์ง่ายๆ ที่หาได้ในครัวเรือน เราจะชวนผู้ร่วมกิจกรรมทุกคนมาเป็นนักวิทยาศาสตร์ เพื่อเจาะลึกและสัมผัสกับดีเอ็นเออย่างใกล้ชิด",
+    hostDepartment: "ชีวเคมี",
+    hostDepartmentAbbr: "BioChem",
+    capacity: 20,
+    image: "/logo.png",
+    slots: [
+      {
+        round: 1,
+        date: new Date("2025-10-18"),
+        start: new Time(11, 15),
+        end: new Time(12, 15),
+      },
+      {
+        round: 2,
+        date: new Date("2025-10-18"),
+        start: new Time(13, 30),
+        end: new Time(14, 30),
+      },
+    ],
+  },
+];
---
+++ src/db/seed/workshops.ts
@@ -0,0 +1,82 @@
+import { workshops } from "@src/data/workshops";
+import { getLocalD1DB } from "@src/db/getDbFile";
+import { createClient } from "@libsql/client";
+import { schema } from "../schema";
+import { drizzle } from "drizzle-orm/libsql";
+import { eq } from "drizzle-orm";
+import { id } from "zod/v4/locales";
+
+async function main() {
+  const db = drizzle({
+    client: createClient({
+      url: getLocalD1DB(),
+    }),
+    schema,
+  });
+
+  for (const workshop of workshops) {
+    console.log(`🌟 Inserting workshop: ${workshop.title}`);
+    await db.transaction(async (tx) => {
+      const existingWorkshop = await tx
+        .select({
+          id: schema.workshops.id,
+        })
+        .from(schema.workshops)
+        .where(eq(schema.workshops.id, workshop.id))
+        .get();
+      if (existingWorkshop) {
+        console.log(`Workshop ${workshop.title} already exists, removing...`);
+
+        await tx
+          .delete(schema.workshopTimeSlots)
+          .where(eq(schema.workshopTimeSlots.workshopId, workshop.id))
+          .returning()
+          .all();
+
+        await tx
+          .delete(schema.workshops)
+          .where(eq(schema.workshops.id, workshop.id))
+          .returning()
+          .get();
+
+        console.log(`Removed existing workshop ${workshop.title}`);
+      }
+
+      await tx
+        .insert(schema.workshops)
+        .values({
+          id: workshop.id,
+          title: workshop.title,
+          description: workshop.description,
+          hostDepartment: workshop.hostDepartment,
+          hostDepartmentAbbr: workshop.hostDepartmentAbbr,
+          image: workshop.image,
+          capacity: workshop.capacity,
+        })
+        .returning()
+        .get();
+
+      for (const slot of workshop.slots) {
+        const result = await tx
+          .insert(schema.workshopTimeSlots)
+          .values({
+            workshopId: workshop.id,
+            roundNumber: slot.round,
+            startTime: slot.start.toString(),
+            endTime: slot.end.toString(),
+            date: slot.date,
+          })
+          .returning()
+          .get();
+        console.log(
+          `  Inserted slot: ${slot.round} (${slot.date.toDateString()} ${slot.start.toString()} - ${slot.end.toString()})`
+        );
+      }
+
+      console.log(`Inserted workshop: ${workshop.title}`);
+      console.log("------------------------------");
+    });
+  }
+}
+
+main();
