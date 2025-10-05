aa08db39b0c4e9231a1b2eb98d9f0beaec0256fa
ongsalt <me@ongsa.lt>
2025-10-06 03:15:47 +0700

    more expected interface

--- src/db/model/workshop-registration.ts
+++ src/db/model/workshop-registration.ts
@@ -6,12 +6,18 @@ export async function getSelectedWorkshop(): Promise<SelectedWorkshop[]> {
       timeSlotIndex: 1,
       workshopId: "kimchi"
     }
-  ]
+  ];
 }
 
 export async function getRegisteredParticipantCount(workshopId: string) {
   // TODO: implement this
 
   // each element is participant count for each time slot with same index
-  return [1, 0, 4, 50]
+  return [1, 0, 4, 50];
+}
+
+export async function setSelectedWorkshopTimeSlots(workshopId: string, timeSlotIndex: number | undefined) {
+  // ถ้ามีเคยเลือกของ workshopId เดียวกัน ให้ลบของเก่า แล้วเปลี่ยนเป็น timeSlotIndex ที่ pass มา
+  // ถ้า pass undefined มาให้ลบอย่างเดียว
+  // อย่าเช็ค if (timeSlotIndex) pls do  if (timeSlotIndex === undefined)
 }
