899aff7e70feb977b764b480c8f3117eb088aed6
ongsalt <ongsa.srp.mkm@gmail.com>
2025-10-17 04:31:11 +0700

    feat: enhance notification handling with specific event responses

--- src/components/notification-receiver.svelte
+++ src/components/notification-receiver.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { boothCheckpoints } from "@src/data/checkpoints";
+  import { workshops } from "@src/data/workshops";
   import { onNotify, type UserEvent } from "@src/notification/client";
-  import { UserEventMaptoast } from "svelte-sonner";
+  import { toast, Toaster } from "svelte-sonner";
 
   interface Props {
     wsUrl: string;
@@ -19,7 +21,40 @@
     latestEvent = event;
 
     // todo: map eventUserEventMap
-    toast(JSON.stringify(event));
+    switch (event.type) {
+      case "registrar-checkin":
+        toast.success("เช็คอินเข้างานสำเร็จ");
+        break;
+      case "booth-checkin":
+        const booth = boothCheckpoints.find((it) => it.id === event.boothId)!;
+        toast.success(`เช็คอินบูธสำเร็จ`, {
+          description: `${booth.name}`,
+        });
+        break;
+      case "workshop-checkin":
+        const workshop = workshops.find((it) => it.id === event.workshopId)!;
+        const slot = workshop.slots.find(
+          (it) => it.round === event.roundNumber,
+        )!;
+        toast.success(`เช็คอินเวิร์กช็อปสำเร็จ`, {
+          description: `${workshop.title} รอบ ${slot.start.toString()}`,
+        });
+        break;
+      case "workshop-onsite-participate":
+        const workshop2 = workshops.find((it) => it.id === event.workshopId)!;
+        const slot2 = workshop2.slots.find(
+          (it) => it.round === event.roundNumber,
+        )!;
+        toast.success(`ลงทะเบียนเข้าร่วมเวิร์กช็อปสำเร็จ`, {
+          description: `${workshop2.title} รอบ ${slot2.start.toString()}`,
+        });
+        break;
+      case "redeem":
+        toast.success(`ยืนยันรับรางวัลสำเร็จ`, {
+          description: JSON.stringify(event),
+        });
+        break;
+    }
   }
 </script>
 
