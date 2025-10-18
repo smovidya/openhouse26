be2ae73dd6c9046c6efb9c7eef4a5ec4b1ddcdf9
ongsalt <ongsa.srp.mkm@gmail.com>
2025-10-18 07:59:43 +0700

    feat: add rewardActions to server exports for improved functionality

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,7 @@ import * as workshopActions from "./workshop";
 import * as devActions from "./dev";
 import * as checkinActions from "./checkin";
 import * as adminActions from "./admin";
+import * as rewardActions from "./reward";
 
 export const server = {
   ...participantActions,
@@ -10,4 +11,5 @@ export const server = {
   ...devActions,
   ...checkinActions,
   ...adminActions,
+  ...rewardActions
 };
