f1a8a3862b697b8426e5cc6b57525bd3377e8dd8
Patsagorn <patsagorn_y@outlook.com>
2025-10-18 16:24:46 +0700

    refactor: remove syncGoogleSheetWithDatabase workflow and related configurations

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -1,7 +1,6 @@
 import { handle } from "@astrojs/cloudflare/handler";
 import type { SSRManifest } from "astro";
 import { App } from "astro/app";
-import { syncGoogleSheetWithDatabase } from "./export-google-sheet";
 
 export function createExports(manifest: SSRManifest) {
   const app = new App(manifest);
@@ -10,11 +9,6 @@ export function createExports(manifest: SSRManifest) {
       async fetch(request, env, ctx) {
         return handle(manifest, app, request as any, env as any, ctx);
       },
-      async scheduled(event, env, ctx) {
-        console.log("Scheduled event triggered:", event);
-        await env.SyncGoogleSheetWithDatabase.create();
-      },
     } satisfies ExportedHandler<Env>,
-    syncGoogleSheetWithDatabase,
   };
 }
