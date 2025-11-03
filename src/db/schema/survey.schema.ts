3023c919c6ad540455c60185c10580a384a5d3e1
Patsagorn <patsagorn_y@outlook.com>
2025-11-03 21:26:18 +0700

    feat: add isNameConfirmed and certId fields to surveys schema with default ID generation

--- src/db/schema/survey.schema.ts
+++ src/db/schema/survey.schema.ts
@@ -3,6 +3,12 @@ import * as t from "drizzle-orm/sqlite-core";
 import { id, timestamps } from "./helper";
 import { relations } from "drizzle-orm";
 import { participants } from "./participant.schema";
+import { customAlphabet } from "nanoid";
+
+const createId = customAlphabet(
+  "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  8,
+);
 
 export const surveys = sqliteTable("surveys", {
   ...id,
@@ -11,6 +17,18 @@ export const surveys = sqliteTable("surveys", {
     .text("responses", {
       mode: "json",
     })
+    .notNull(),
+  isNameConfirmed: t
+    .int("is_name_confirmed", {
+      mode: "boolean",
+    })
+    .notNull()
+    .default(false),
+  nameInCert: t.text("name_in_cert"),
+  certId: t
+    .text("cert_id")
+    .unique()
+    .$defaultFn(() => createId())
     .notNull(),
   ...timestamps,
 });
