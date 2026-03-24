b6a9f2fc47d43980a5b23c070fd3efee1ed8db1f
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-03-25 04:19:36 +0700

    refactor: monor fix

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,53 +1,68 @@
 <script lang="ts">
-    import QrIcon from "@src/assets/icons/qrcode.svg";
-    import LogoNoCircle from "@src/assets/logo-no-circle.png";
-    import clsx from "clsx";
-    import { Hamburger } from 'svelte-hamburgers';
+  import QrIcon from "@src/assets/icons/qrcode.svg";
+  import LogoNoCircle from "@src/assets/logo-no-circle.png";
+  import clsx from "clsx";
+  import { Hamburger } from "svelte-hamburgers";
+  import { fly, slide } from "svelte/transition";
 
-    interface Props {
-        class?: string;
-    }
-    let { class: className }: Props = $props();
+  interface Props {
+    class?: string;
+  }
+  let { class: className }: Props = $props();
 
-    let menuOpen = $state(false);
-    const menuList = [
-        {title:"LOBBY",icon:"/nowath/icon/hotel.svg", path:"/"},
-        {title:"WEBSITES",icon:"/nowath/icon/globe.svg", path:"/websites"},
-        {title:"WORKSHOPS",icon:"/nowath/icon/flask-round.svg", path:"/workshops"},
-        {title:"RECHECK",icon:"/nowath/icon/redeem.svg", path:"/"},
-        {title:"REDEEM",icon:"/nowath/icon/file-badge.svg", path:"/"},
-    ]
+  let menuOpen = $state(false);
+  const menuList = [
+    { title: "LOBBY", icon: "/nowath/icon/hotel.svg", path: "/" },
+    { title: "WEBSITES", icon: "/nowath/icon/globe.svg", path: "/websites" },
+    {
+      title: "WORKSHOPS",
+      icon: "/nowath/icon/flask-round.svg",
+      path: "/workshops",
+    },
+    { title: "RECHECK", icon: "/nowath/icon/redeem.svg", path: "/" },
+    { title: "REDEEM", icon: "/nowath/icon/file-badge.svg", path: "/" },
+  ];
 </script>
 
-<nav class={clsx("w-full z-100 h-20 relative shadow-md shadow-black/30 bg-(--background)", className)}>
-    <div class="flex justify-between items-center w-full h-full px-4">
-        <a href="/" class="z-10">
-            <img src={`/nowath/logo/logo1.svg`} alt="logo" />
-        </a>
-        <div class="flex z-10 relative h-full items-center text-xs font-serif">
-            <Hamburger  type="slider" bind:open={menuOpen} onclick={() => console.log("fawd")} --color="#F7F5EA" />
-        </div>
+<nav
+  class={clsx(
+    "w-full z-100 h-20 relative shadow-md shadow-black/30 bg-(--background)",
+    className,
+  )}
+>
+  <div class="flex justify-between items-center w-full h-full px-4">
+    <a href="/" class="z-10">
+      <img src={`/nowath/logo/logo1.svg`} alt="logo" />
+    </a>
+    <div class="flex z-10 relative h-full items-center text-xs font-serif">
+      <Hamburger type="slider" bind:open={menuOpen} --color="#F7F5EA" />
     </div>
+  </div>
 </nav>
 
 {#if menuOpen}
-    <div class="fixed w-full max-w-125 z-99">
-        {#each menuList as item}
-            <a href={item.path} class="flex gap-2 px-4 h-20 items-center bg-(--color-2) group hover:bg-(--background) transition-all">
-                <img
-                    src={item.icon}
-                    class=" transition-all"
-                    alt={item.icon.split("/")[-1]}
-                >
-                <span class="group-hover:text-(--text-white) text-(--text-dark) transition-all">{item.title}</span>
-            </a>
-        {/each}
-    </div>
+  <div class="fixed w-full max-w-125 z-99" transition:slide={{ duration: 300 }}>
+    {#each menuList as item}
+      <a
+        href={item.path}
+        class="flex gap-2 px-4 h-20 items-center bg-(--color-2) group hover:bg-(--background) transition-all"
+      >
+        <img
+          src={item.icon}
+          class="transition-all"
+          alt={item.icon.split("/")[-1]}
+        />
+        <span
+          class="group-hover:text-(--text-white) text-(--text-dark) transition-all font-bold"
+          >{item.title}</span
+        >
+      </a>
+    {/each}
+  </div>
 {/if}
 
-
 <style>
-.group:hover img {
+  .group:hover img {
     filter: brightness(0) invert(1);
-}
+  }
 </style>
