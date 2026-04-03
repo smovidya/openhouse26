0f4cfebd8a99b433d985264f1199688b4c72d9a3
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-04-03 17:57:30 +0700

    refactor: remove unnecessary console logs from fetch handler

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -3,12 +3,10 @@ import { handle } from "@astrojs/cloudflare/handler";
 export default {
   async fetch(request, env, ctx) {
     const requestURL = new URL(request.url);
-    console.log(`Received request for ${requestURL.pathname}`);
     if (requestURL.pathname.startsWith("/certs/")) {
       if (requestURL.searchParams.get("s") !== env.RENDER_SECRET) {
         return new Response("Unauthorized", { status: 401 });
       }
-      console.log(`Serving cert template: ${requestURL.pathname}`);
       return env.ASSETS.fetch(request);
     }
     return handle(request, env, ctx);
